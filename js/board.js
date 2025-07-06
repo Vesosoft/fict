0bdc078985891d6db48a40ff7113ea0fe85e28b9
Veselin Ignatov <49739278+Vesosoft@users.noreply.github.com>
2025-07-06 23:36:25 +0300

    Update board.js

--- js/board.js
+++ js/board.js
@@ -1,1 +1,9 @@
+import { Chess } from '../libs/chess.min.js'; // ако chess.js е модул
+// ако не — този ред не е нужен
 
+const board = Chessboard('board', {
+  position: 'start',
+  draggable: false,
+  showNotation: true,
+  pieceTheme: 'libs/img/chesspieces/wikipedia/{piece}.png'
+});
