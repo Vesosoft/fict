2ac34790a8545330f4aef418a1cb79496cbd3d3c
Veselin Ignatov <49739278+Vesosoft@users.noreply.github.com>
2025-07-25 22:02:28 +0300

    Update butons.js

--- js/butons.js
+++ js/butons.js
@@ -1,9 +1,9 @@
-import { setupAuto } from './butons/auto.js';
-import { setupClear } from './butons/clear.js';
-import { setupLoad } from './butons/load.js';
-import { setupNext } from './butons/next.js';
-import { setupPrev } from './butons/prev.js';
-import { setupSave } from './butons/save.js';
+import { setupAuto } from '../butons/auto.js';
+import { setupClear } from '../butons/clear.js';
+import { setupLoad } from '../butons/load.js';
+import { setupNext } from '../butons/next.js';
+import { setupPrev } from '../butons/prev.js';
+import { setupSave } from '../butons/save.js';
 
 export function initButtons(game, board) {
   setupAuto(game, board);
