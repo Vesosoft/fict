3f4641ff1140e38a97625b5ee3e9e06c2cc5d56b
Veselin Ignatov <49739278+Vesosoft@users.noreply.github.com>
2025-07-26 21:04:55 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-import { createBoard } from './board.js';
+import { createControlButtons } from './butons.js';
+
+const board = Chessboard('board', {
+  draggable: true,
+  position: 'start',
+  showNotation: true
+});
+
+createControlButtons(board);
+
+/* import { createBoard } from './board.js';
 import { setupFlipButton } from './js/Buttons.js';
 
 createBoard();
 setupFlipButton();
-/*// main.js
+// main.js
 import { createBoard } from './board.js';
 import { setupControls } from './controls.js';
 import { setupRotateButton } from './rotate.js';
@@ -12,4 +22,4 @@ document.addEventListener('DOMContentLoaded', () => {
   createBoard('board');              // Създава дъската в елемент с ID 'board'
   setupControls();                   // Настройва контролите (бутоните за ходове и др.)
   setupRotateButton('rotateBtn');   // Активира бутона за завъртане на дъската
-})*/
+}) */
