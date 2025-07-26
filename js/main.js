d5aa1f657adc00b7149139d63fda661e2824a128
Veselin Ignatov <49739278+Vesosoft@users.noreply.github.com>
2025-07-26 04:25:23 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-// main.js
+import { createBoard } from './board.js'
+import { initControls } from './controls.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const board = initBoard()
+  initControls(board)
+})
+/*// main.js
 import { createBoard } from './board.js';
 import { setupControls } from './controls.js';
 import { setupRotateButton } from './rotate.js';
@@ -7,4 +14,4 @@ document.addEventListener('DOMContentLoaded', () => {
   createBoard('board');              // Създава дъската в елемент с ID 'board'
   setupControls();                   // Настройва контролите (бутоните за ходове и др.)
   setupRotateButton('rotateBtn');   // Активира бутона за завъртане на дъската
-});
+})*/
