01fc01e387ef6f6da4e7395b65746d0b5726f1fd
Veselin Ignatov <49739278+Vesosoft@users.noreply.github.com>
2025-07-31 06:12:37 +0300

    Update loadHTML.js

--- js/utils/loadHTML.js
+++ js/utils/loadHTML.js
@@ -1,5 +1,32 @@
 // js/utils/loasPGN.js
-
+/*export default function loadHTML(url, selector) {
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Грешка при зареждане на ${url}: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then(html => {
+      document.querySelector(selector).innerHTML = html;
+    })
+    .catch(error => {
+      console.error("Грешка при зареждане на HTML:", error);
+    });
+    }*/
+// Вариант с async/await:
+export default async function loadHTML(url, selector) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Грешка при зареждане на ${url}: ${response.status}`);
+    }
+    const html = await response.text();
+    document.querySelector(selector).innerHTML = html;
+  } catch (error) {
+    console.error("Грешка при зареждане на HTML:", error);
+  }
+}
 // Изчакваме цялата страница да се зареди
 window.addEventListener('DOMContentLoaded', () => {
   const iframe = document.getElementById('loadFrame');
